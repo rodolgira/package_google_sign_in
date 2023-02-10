699677710f6d292408510c217eb102b37bdc2666
Rodolfo Giraldo <rodolfo.giraldo@pragma.com.co>
2023-02-10 15:20:58 -0500

    Agregue export al archivo principal

--- lib/package_google_sign_in.dart
+++ lib/package_google_sign_in.dart
@@ -1,4 +1,4 @@
 library package_google_sign_in;
 
-export 'provider/provider_google_sign_in.dart';
-export 'service/service_google_sign_in.dart';
+export 'package:package_google_sign_in/provider/provider_google_sign_in.dart';
+export 'package:package_google_sign_in/service/service_google_sign_in.dart';
