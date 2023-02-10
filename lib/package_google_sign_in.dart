644c26e238daadebe58d0d5a07e94de03a42b513
Rodolfo Giraldo <rodolfo.giraldo@pragma.com.co>
2023-02-10 15:18:18 -0500

    Agregue export al archivo principal

--- lib/package_google_sign_in.dart
+++ lib/package_google_sign_in.dart
@@ -1,7 +1,4 @@
 library package_google_sign_in;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'provider/provider_google_sign_in.dart';
+export 'service/service_google_sign_in.dart';
